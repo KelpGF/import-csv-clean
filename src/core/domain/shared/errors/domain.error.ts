88ea7972b08e66db4912d0ad58e004cf1f1b3cfa
KelpGF <kelpgf@gmail.com>
2024-07-02 23:10:30 -0300

    feat: add create factory method on domain erro

--- src/core/domain/shared/errors/domain.error.ts
+++ src/core/domain/shared/errors/domain.error.ts
@@ -3,4 +3,8 @@ export class DomainError extends Error {
     super(message);
     this.name = "DomainError";
   }
+
+  static create(message: string): DomainError {
+    return new DomainError(message);
+  }
 }
