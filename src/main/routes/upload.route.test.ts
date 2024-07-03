376eb3cd3bbff886ca8ea5d7d60d01956de755d9
KelpGF <kelpgf@gmail.com>
2024-07-03 14:37:29 -0300

    test: ensure /upload process files faster

--- src/main/routes/upload.route.test.ts
+++ src/main/routes/upload.route.test.ts
@@ -5,9 +5,25 @@ describe("Upload route", () => {
   test("Should return 200 on up", async () => {
     await request(setUpApp())
       .post("/upload")
-      .attach("file", "input-test.csv")
+      .attach("file", "csv-files/input-test.csv")
       .expect(200);
   });
+
+  test("Should return 200 and with execution time lower than 1 minute", async () => {
+    const startDate = Date.now();
+
+    const response = await request(setUpApp())
+      .post("/upload")
+      .attach("file", "csv-files/input.csv")
+      .expect(200);
+
+    const endDate = Date.now();
+
+    expect(response.body.message).toBe(
+      "File uploaded successfully! We are processing the data",
+    );
+    expect(endDate - startDate).toBeLessThan(60000);
+  }, 60000);
 
   test("Should return 400 if no file is uploaded", async () => {
     await request(setUpApp()).post("/upload").expect(400);
